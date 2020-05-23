37c097885afcf02e126c9c684e09091ae35463a0
thanhlongnguyen140499 <thanhlongnguyen140499@gmail.com>
2020-05-23 11:51:18 +0700

    Assigment3

---
+++ Gemfile
@@ -0,0 +1,43 @@
+source 'https://rubygems.org'
+
+git_source(:github) do |repo_name|
+  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
+  "https://github.com/#{repo_name}.git"
+end
+
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '~> 5.1.6'
+#gem 'sqlite3'
+gem 'puma', '~> 3.7'
+gem 'sass-rails', '~> 5.0'
+gem 'bootstrap-sass','3.4.1'
+gem 'webpacker', '~> 4.0'
+gem 'rails-controller-testing'
+gem 'bcrypt','3.1.13'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.2'
+gem 'turbolinks', '~> 5'
+gem 'jbuilder', '~> 2.5'
+group :development, :test do
+  gem 'sqlite3', '1.3.13'
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'capybara', '~> 2.13'
+  gem 'selenium-webdriver'
+  gem 'guard' # NOTE: this is necessary in newer versions
+  gem 'guard-minitest'
+end
+
+group :development do
+  gem 'web-console', '>= 3.3.0'
+  gem 'listen', '>= 3.0.5', '< 3.2'
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+group :production do
+  gem 'pg', '0.20.0'
+end
+
+# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ app/models/user.rb
@@ -0,0 +1,46 @@
+class User < ApplicationRecord
+    #before_save {self.email=email.downcase }
+    attr_accessor :remember_token
+    before_save { email.downcase! }
+    validates:name,presence:true
+    validates:email,presence:true
+    validates:name,presence:true,length: {maximum:50}
+    VALID_EMAIL_REGEX=  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates:email,presence:true,length: {maximum:255},    format: {with: VALID_EMAIL_REGEX},
+            uniqueness:true
+    has_secure_password
+    validates:password,presence:true,length: {minimum:6}
+    def remember
+      #remember_token = new_token
+      self.remember_token = User.new_token
+     update_attribute(:remember_digest, User.digest(remember_token))
+    end
+    class << self
+    # Returns the hash digest of the given string.
+    #def self.digest(string)
+    #def User.digest(string)
+      def digest(string)
+          cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                         BCrypt::Engine.cost
+                         BCrypt::Password.create(string, cost: cost)
+      end 
+    # Returns a random token.
+    def new_token
+    #def self.new_token
+    #def User.new_token 
+      SecureRandom.urlsafe_base64
+     end
+    # Remembers a user in the database for use in persistent sessions.
+    
+    # Returns true if the given token matches the digest.
+    
+  end
+  def authenticated?(remember_token) 
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+    # Forgets a user.
+    def forget 
+        update_attribute(:remember_digest, nil)
+    end
+end
